1a95d4c8312ab687e4cad32dc5f5ae423cbc9a22
My Lhassane <mastinoz_7@hotmail.com>
2022-08-13 21:31:35 +0100

    Fixed issue on close the Menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -42,7 +42,7 @@ export default function Navbar() {
             <AiOutlineMenu size={25} />
           </div>
           <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
-            <div className={nav ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-2 ease-in duration-500' : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'}>
+            <div className={nav ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-2 ease-in duration-500' : 'fixed left-[-100%] top-0 p-2 ease-in duration-500'}>
               <div>
                 <div className='flex w-full items-center justify-between'>
                   <Image src={logo} className="rounded-full" width="56" height="56" alt="/" />
