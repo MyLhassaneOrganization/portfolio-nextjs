2d625bc0b73f0becd83dbabd34aa59edd1682150
My Lhassane <mastinoz_7@hotmail.com>
2022-08-15 17:59:31 +0100

    Added Dynamic Styles on Navbar Based on Route

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,12 +5,33 @@ import Link from "next/link";
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { useRouter } from 'next/router';
 
 export default function Navbar() {
 
   const [nav, setNav] = useState(false)
 
   const [ shadow, setShadow ] = useState(false)
+
+  const [ navBg, setNavBg ] = useState('#ecf0f3')
+  const [ linkColor, setLinkColor ] = useState('#1f2937')
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (
+      router.asPath === '/property' ||
+      router.asPath === '/crypto' ||
+      router.asPath === '/netflix' ||
+      router.asPath === '/twitch'
+    ) {
+      setNavBg('transparent')
+      setLinkColor('#ecf0f3')
+    } else {
+      setNavBg('#ecf0f3')
+      setLinkColor('#1f2937')
+    }
+  },[router])
 
   const handleNav = () => {
 
@@ -29,14 +50,14 @@ export default function Navbar() {
   },[])
 
   return (
-    <div className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
+    <div style={{backgroundColor: `${navBg}`}} className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
         <Link href='/'>
           <Image className="rounded-full" src={logo} alt="Logo" width="62" height="62" />
         </Link>
         <div>
 
-          <ul className='hidden md:flex'>
+          <ul style={{color: `${linkColor}`}} className='hidden md:flex'>
             <Link href="/">
               <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
             </Link>
