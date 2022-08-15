b7ea794f58d04d434aefaec03963f0e9d64db487
My Lhassane <mastinoz_7@hotmail.com>
2022-08-15 19:21:53 +0100

    Added my picture

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Image from 'next/image';
+import iam from '../public/assets/iam.jpg';
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
@@ -10,10 +12,14 @@ const Main = () => {
       {/* Main Container */}
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
-          <p className='uppercase text-sm tracking-widest text-gray-600'>LET&apos;S BUILD SOMETHING TOGETHER</p>
+          <div className='bg-red'>
+            <Image className="rounded-full" src={iam} alt="My Picture" width="100" height="100" />
+          </div>
+          <p className='uppercase text-sm tracking-widest text-gray-600 mt-4'>LET&apos;S BUILD SOMETHING TOGETHER</p>
           <h1 className='py-4 text-gray-700'>Hi, Im hassan <span className='text-[#5651e5]'>Alaoui</span></h1>
           <h1 className='py-2 text-gray-700'>A Front-End Web Developer</h1>
-          <p>I&apos;m a fron-end web developer spicializing in building (and occasionally designing) exceptionally digital experiences. Currently, I&apos;m focused on building responsive front-end web applications while learning back-end technologies.</p>
+          <p>I&apos;m a fron-end web developer spicializing in building (and occasionally designing) exceptionally digital experiences.</p>
+          <p>Currently, I&apos;m focused on building responsive front-end web applications while learning back-end technologies.</p>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
             <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
               <FaLinkedinIn />
