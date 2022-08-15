8a6724d38a4b7b27026440b0ad6730d49b147f83
My Lhassane <mastinoz_7@hotmail.com>
2022-08-15 11:58:49 +0100

    Added Smooth Scrolling on the mobile menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -60,7 +60,9 @@ export default function Navbar() {
             <div className={nav ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-2 ease-in duration-500' : 'fixed left-[-100%] top-0 p-2 ease-in duration-500'}>
               <div>
                 <div className='flex w-full items-center justify-between'>
-                  <Image src={logo} className="rounded-full" width="56" height="56" alt="/" />
+                  <Link href='/'>
+                    <Image src={logo} className="rounded-full" width="56" height="56" alt="/" />
+                  </Link>
                   <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                     <AiOutlineClose />
                   </div>
@@ -72,19 +74,19 @@ export default function Navbar() {
               <div className='py-4 flex flex-col'>
                 <ul className='uppercase'>
                   <Link href="/">
-                    <li className='py-4 text-sm'>Home</li>
+                    <li onClick={()=> setNav(false)} className='py-4 text-sm'>Home</li>
                   </Link>
-                  <Link href="/about">
-                    <li className='py-4 text-sm'>About</li>
+                  <Link href="/#about">
+                    <li onClick={()=> setNav(false)} className='py-4 text-sm'>About</li>
                   </Link>
-                  <Link href="/skills">
-                    <li className='py-4 text-sm'>Skills</li>
+                  <Link href="/#skills">
+                    <li onClick={()=> setNav(false)} className='py-4 text-sm'>Skills</li>
                   </Link>
-                  <Link href="/projects">
-                    <li className='py-4 text-sm'>Projects</li>
+                  <Link href="/#projects">
+                    <li onClick={()=> setNav(false)} className='py-4 text-sm'>Projects</li>
                   </Link>
-                  <Link href="/contact">
-                    <li className='py-4 text-sm'>Contact</li>
+                  <Link href="/#contact">
+                    <li onClick={()=> setNav(false)} className='py-4 text-sm'>Contact</li>
                   </Link>
                 </ul>
                 <div className='pt-40'>
